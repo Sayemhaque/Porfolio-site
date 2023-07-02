9c64613517241a064a686dce4aaf63c07da516be
sayemhaque <mdsayemofficial1000@gmail.com>
2023-07-02 20:37:45 +0600

    added 3 projects

--- src/components/Works.js
+++ src/components/Works.js
@@ -18,7 +18,7 @@ const Works = () => {
         </p>
       </div>
       <div className="max-w-[1000px] mx-auto py-10">
-        <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 gap-5 ">
+        <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-2 gap-5 ">
           {project.map((x) => {
             return (
               <>
@@ -28,23 +28,23 @@ const Works = () => {
                   className="bg-[#EDF5E1] bg-opacity-30  shadow-xl">
                   <div className="flex flex-col  space-y-3 ">
                     <img className="w-full" src={x.img} alt="" />
-                    <p className="text-[16px] px-5 font-bold ">
+                    <p className="text-2xl px-5 font-bold ">
                       <i class="fa-solid fa-arrow-right"></i> {x.title}
                     </p>
-                    <p className="px-5 text-zinc-700">
-                      Technology: {x.tech[0]},{x.tech[1]},{x.tech[2]},{x.tech[3]}
+                    <p className="px-5 text-justify">{x.details}</p>
+                    <p className="px-5 text-zinc-800 ">
+                      Technology:  {x.tech.join(" | ")}
                     </p>
-                    <div className="flex justify-between px-5 pb-4">
-                      <button className="bg-zinc-800 bg-opacity-80 py-1 px-5 hover:bg-opacity-100 text-[#EDF5E1]">
-                        <a href={x.githubLink} target="blank">
-                          <i class="fa-brands fa-github"></i> Github
+                    <div className="flex flex-col gap-5 text-center md:gap-0 md:flex-row justify-between px-5 pb-4">
+                        <a className="bg-zinc-800   bg-opacity-80 py-1 px-5 hover:bg-opacity-100 text-[#EDF5E1]" href={x.githubLinkClient} target="blank">
+                          <i class="fa-brands fa-github"></i> Github Client
                         </a>
-                      </button>
-                      <button className="bg-zinc-800 bg-opacity-80 py-1 px-5 hover:bg-opacity-100 text-[#EDF5E1]">
-                        <a href={x.liveLink} target="blank">
+                        <a  className="bg-zinc-800  bg-opacity-80 py-1 px-5 hover:bg-opacity-100 text-[#EDF5E1]" href={x.githubLinkServer} target="blank">
+                          <i class="fa-brands fa-github"></i> Github Server
+                        </a>
+                        <a className="bg-zinc-800 bg-opacity-80 py-1 px-5 hover:bg-opacity-100 text-[#EDF5E1]" href={x.liveLink} target="blank">
                           <i class="fa-solid fa-eye"></i> Live
                         </a>
-                      </button>
                     </div>
                   </div>
                 </div>
--- src/data,.js
+++ src/data,.js
@@ -2,6 +2,9 @@ import FoodWebsiteImg from './images/Untitled design.jpg';
 import G3ArchitectsImg from './images/Untitled design (2).jpg';
 import DonateTodayImg from './images/Untitled design (3).jpg';
 import CoffeShopImg from './images/Untitled design (4).jpg';
+import shaporiItalianoImg from './images/recipe-app.png';
+import toyCastleImg from './images/toy-castle.png';
+import sportifyImg from './images/sportify-camp(2).png';
 
 
 export const frontedSkills = [
@@ -36,42 +39,75 @@ export const learningNow = [
 ]
 
 export const projects = [
-    {
-        id: 1,
-        title:"Mobile Resonsive Burger shop website using vanila Html and css",
-        img: FoodWebsiteImg,
-        end: "Frontend",
-        tech: ["Html5", "css3", "Grid" , "Flexbox", "Media Query"],
-        liveLink: "https://sayemhaque.github.io/Food-website2/",
-        githubLink: "https://github.com/Sayemhaque/Food-website2"
-    },
+    // {
+    //     id: 1,
+    //     title:"Mobile Resonsive Burger shop website using vanila Html and css",
+    //     img: FoodWebsiteImg,
+    //     end: "Frontend",
+    //     tech: ["Html5", "css3", "Grid" , "Flexbox", "Media Query"],
+    //     liveLink: "https://sayemhaque.github.io/Food-website2/",
+    //     githubLinkClient: "https://github.com/Sayemhaque/Food-website2"
+    // },
 
+    // {
+    //     id: 2,
+    //     title:"Mobile Resonsive website using vanila Html and css",
+    //     img: G3ArchitectsImg,
+    //     end: "Frontend",
+    //     tech: ["Html5", "css3", "Grid" , "Flexbox", "Media Query"],
+    //     liveLink: "https://sayemhaque.github.io/G3-Architects/",
+    //     githubLinkClient: "https://github.com/Sayemhaque/G3-Architects"
+    // },
+    // {
+    //     id: 3, 
+    //     title: "Mobile Resonsive website using vanila Html and css",
+    //     img: DonateTodayImg,
+    //     end: "Frontend",
+    //     tech: ["Html5", "css3", "Grid" , "Flexbox", "Media Query"],
+    //     liveLink: "https://sayemhaque.github.io/Donate-Today/",
+    //     githubLinkClient: "https://github.com/Sayemhaque/Donate-Today" 
+    // },
+    // {
+    //     id: 4, 
+    //     title: "Resonsive coffee shop website using  Html,Bootstrap and css.",
+    //     img: CoffeShopImg,
+    //     end: "Frontend",
+    //     tech: ["Html5", "css3", "Bootstrap"],
+    //     liveLink: "https://sayemhaque.github.io/Coffee-shop-Bootstrap/",
+    //     githubLinkClient: "https://github.com/Sayemhaque/Coffee-shop-Bootstrap" 
+    // },
     {
-        id: 2,
-        title:"Mobile Resonsive website using vanila Html and css",
-        img: G3ArchitectsImg,
-        end: "Frontend",
-        tech: ["Html5", "css3", "Grid" , "Flexbox", "Media Query"],
-        liveLink: "https://sayemhaque.github.io/G3-Architects/",
-        githubLink: "https://github.com/Sayemhaque/G3-Architects"
+        id: 5, 
+        title: "Sapori Italiano",
+        img: shaporiItalianoImg,
+        end: "MERN STACK",
+        details:"This single page application utilizes dynamic routing and incorporates Firebase authentication. The Chef Details page is protected, meaning that access to it is restricted. If a user is logged in and clicks on the View Recipe button, they will be redirected to the Chef Details page. However, if the user is not logged in and attempts to access the recipe details page, they will be redirected to the login page. To handle the backend functionality, Express.js is used, and dummy JSON data is obtained from the backend. Additionally, the website optimizes image performance by employing react-lazy-load",
+        tech: ["React Js","Tailwind css","DaisyUi","React Router Dom","Express js","MongoDB","Firebase","React-Lazy-Image"],
+        liveLink: "https://repice-app-6cac9.web.app/",
+        githubLinkClient: "https://github.com/Sayemhaque/chef-website" ,
+        githubLinkServer: "https://github.com/Sayemhaque/chef-website-backend" 
     },
     {
-        id: 3, 
-        title: "Mobile Resonsive website using vanila Html and css",
-        img: DonateTodayImg,
-        end: "Frontend",
-        tech: ["Html5", "css3", "Grid" , "Flexbox", "Media Query"],
-        liveLink: "https://sayemhaque.github.io/Donate-Today/",
-        githubLink: "https://github.com/Sayemhaque/Donate-Today" 
+        id: 6, 
+        title: "TOY Castle",
+        img: toyCastleImg,
+        stack: "MERN STACK",
+        details:"The Toy Castle web application is a full stack application built using the MERN stack (MongoDB, Express.js, React.js, and Node.js). The website utilizes Firebase for authentication purposes. Users are required to register to explore all features of the website. Once logged in, users can add toys and access protected pages such as the toy details page. If a user is not logged in and tries to view the details page, they will be redirected to the login page. On the My Toys page, logged-in users can update and delete their own toys, while the All Toys page features a search system allowing users to search for toys by name.",
+        tech: ["React Js","Tailwind css","DaisyUi","React Router Dom","Express js","MongoDB","Firebase","React Tabs","react-toastify"],
+        liveLink: "https://toy-castle-409e5.web.app/",
+        githubLinkClient: "https://github.com/Sayemhaque/toy-castle-client" ,
+        githubLinkServer: "https://github.com/Sayemhaque/toy-castle-server" 
     },
     {
-        id: 4, 
-        title: "Resonsive coffee shop website using  Html,Bootstrap and css.",
-        img: CoffeShopImg,
-        end: "Frontend",
-        tech: ["Html5", "css3", "Bootstrap"],
-        liveLink: "https://sayemhaque.github.io/Coffee-shop-Bootstrap/",
-        githubLink: "https://github.com/Sayemhaque/Coffee-shop-Bootstrap" 
+        id: 6, 
+        title: "Sportify Camp",
+        img: sportifyImg,
+        stack: "MERN STACK",
+        details:"Sportify is a feature-rich web application designed specifically for sports enthusiasts. It offers a range of functionalities including JWT authorization for secure user authentication, role-based routing to ensure appropriate access levels, dedicated admin and instructor panels for streamlined management, Firebase authentication for reliable user verification, and seamless payment integration with Stripe. With its comprehensive set of features, Sportify provides a robust platform where users can connect, explore various sports classes, and make online payments with ease and confidence.",
+        tech: ["React Js","Tailwind css","DaisyUi","Axios","React Router Dom","Express js","MongoDB","JWT","Firebase","React Tabs","react-toastify","Stripe","react-hook-form","react-responsive-carousel","react-simple-typewriter","sweetalert2"],
+        liveLink: "https://sportify-app-35c1c.web.app/",
+        githubLinkClient: "https://github.com/Sayemhaque/sportify-camp" ,
+        githubLinkServer: "https://github.com/Sayemhaque/sportify-camp-backend" 
     },
 
 ]
